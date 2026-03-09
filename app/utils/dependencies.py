2b485c5ab1da69584c6f3e35e93b213da820e080
Muhammad <usman.butt131@gmail.com>
2026-03-09 13:51:10 +0100

    including order routes

--- app/utils/dependencies.py
+++ app/utils/dependencies.py
@@ -1,10 +1,9 @@
 from app.db.session import AsyncSession, get_async_db_session
 from fastapi import Depends
 from app.service.interfaces.user_service_interface import IUserService
-
-# from app.service.interfaces.order_interface import IOder
+from app.service.interfaces.order_interface import IOder
 from app.service.user.user_service import UserService
-# from app.service.order.order_service import OrderService
+from app.service.order.order_service import OrderService
 
 
 def get_user_service(
@@ -13,7 +12,7 @@ def get_user_service(
     return UserService(session=session)
 
 
-# def get_order_service(
-#     session: AsyncSession = Depends(get_async_db_session),
-# ) -> IOder:
-#     return OrderService(session=session)
+def get_order_service(
+    session: AsyncSession = Depends(get_async_db_session),
+) -> IOder:
+    return OrderService(session=session)

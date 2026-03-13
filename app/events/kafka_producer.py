f96349c446b12dd1236e5bbe3f1ae9ea22ca8def
Muhammad <usman.butt131@gmail.com>
2026-03-14 00:00:51 +0100

    update kafka

--- app/events/kafka_producer.py
+++ app/events/kafka_producer.py
@@ -2,8 +2,23 @@ from kafka import KafkaProducer
 import json
 
 producer = KafkaProducer(
-    bootstrap_servers="kafka:9092",
+    bootstrap_servers=["kafka:9092"],
     value_serializer=lambda v: json.dumps(v).encode("utf-8"),
+    retries=5,
 )
 
-producer.send("test-topic", {"message": "hello kafka"})
+future = producer.send("test-topic", {"message": "hello kafka"})
+
+try:
+    record_metadata = future.get(timeout=10)
+    print("Sent to:")
+    print("Topic:", record_metadata.topic)
+    print("Partition:", record_metadata.partition)
+    print("Offset:", record_metadata.offset)
+
+except Exception as e:
+    print("Kafka send failed:", e)
+
+finally:
+    producer.flush()
+    producer.close()
